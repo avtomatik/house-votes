cdbd9fe618a531bf79bfb7276b497c8ecd8645ea
Alexander Mikhailov <extroper@yandex.ru>
2025-05-12 14:09:16 +0300

    Extract Config

---
+++ house_votes/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 12 14:01:05 2025
+
+@author: alexandermikhailov
+"""
+
+# =============================================================================
+# https://www.kaggle.com/datasets/devvret/congressional-voting-records?select=house-votes-84.names # noqa: E501
+# =============================================================================
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+
+DATA_DIR = BASE_DIR.joinpath('data')
+
+FILE_NAME = 'vote.txt'
--- house_votes/dataset.py
+++ house_votes/dataset.py
@@ -6,24 +6,12 @@ Created on Sun Jan 12 00:07:27 2025
 @author: alexandermikhailov
 """
 
-from pathlib import Path
-
 import pandas as pd
-
-# =============================================================================
-# https://www.kaggle.com/datasets/devvret/congressional-voting-records?select=house-votes-84.names # noqa: E501
-# =============================================================================
-FILE_NAME = 'vote.txt'
+from config import DATA_DIR, FILE_NAME
 
 kwargs = {
-    'filepath_or_buffer': (
-        Path(__file__).parent.parent
-        .joinpath('data')
-        .joinpath('raw')
-        .joinpath(FILE_NAME)
-    ),
+    'filepath_or_buffer': DATA_DIR.joinpath('raw').joinpath(FILE_NAME),
     'sep': '\t',
-    # 'names': ,
     'index_col': 0,
     'encoding': 'cp1251',
 }
