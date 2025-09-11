ee747c9699f0de9d4d993c23a4d6e6d5a0d1e1d6
Alexander Mikhailov <extroper@yandex.ru>
2025-09-11 16:12:13 +0300

    Refactor dataset loader: use constants.py and canonical column names

---
+++ house_votes/constants.py
@@ -0,0 +1,23 @@
+VOTE_MAPPING = {'да': 1, 'нет': -1, 'воздержался': 0}
+
+PARTY_MAPPING = {'демократ': 0, 'республиканец': 1}  # 0=Democrat, 1=Republican
+
+ENGLISH_COLUMNS = [
+    'Handicapped Infants',
+    'Water Project Cost Sharing',
+    'Adoption of Budget Resolution',
+    'Physician Fee Freeze',
+    'El Salvador Aid',
+    'Religious Groups in Schools',
+    'Anti-Satellite Test Ban',
+    'Aid to Nicaraguan Contras',
+    'MX Missile',
+    'Immigration',
+    'Synfuels Corp Cutback',
+    'Education Spending',
+    'Superfund Right to Sue',
+    'Crime',
+    'Duty-Free Exports',
+    'Export Administration Act / South Africa',
+    'Party'
+]
--- house_votes/dataset.py
+++ house_votes/dataset.py
@@ -7,16 +7,22 @@ Created on Sun Jan 12 00:07:27 2025
 """
 
 import pandas as pd
-from config import DATA_DIR, FILE_NAME
 
-kwargs = {
-    'filepath_or_buffer': DATA_DIR / 'raw' / FILE_NAME,
-    'sep': '\t',
-    'index_col': 0,
-    'encoding': 'cp1251',
-}
+from .config import DATA_DIR, FILE_NAME
+from .constants import ENGLISH_COLUMNS, PARTY_MAPPING, VOTE_MAPPING
 
-df = pd.read_csv(**kwargs)
-df.replace(['да', 'нет'], [1, 0], inplace=True)
-print(df.describe())
+file_path = DATA_DIR / 'raw' / FILE_NAME
+
+df = pd.read_csv(file_path, sep='\t', index_col=0, encoding='cp1251')
+
+vote_cols = df.columns[:-1]
+class_col = df.columns[-1]
+df[vote_cols] = df[vote_cols].map(VOTE_MAPPING.get)
+df[class_col] = df[class_col].map(PARTY_MAPPING)
+
+df.index.name = 'id'
+df.columns = ENGLISH_COLUMNS
+
+print(df.head())
 df.info()
+print(df.describe())
